40d42f34cdb1ee043bb0d0a17fcab0119849123f
Giovanni_Bono <bono.giovanni00@gmail.com>
2024-01-17 13:36:10 +0100

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,71 @@
-const increse = document.getElementById("increment");
-const decrese  = document.getElementById("decrement");
-var counterElement = document.getElementById("counter");
-
 /**
- * Listen to increment events
+ * This function return a HTML button by giving it a label and a parameter, in this case a function 
+ * @param {string} label 
+ * @param {parameter} clickHandler 
+ * @returns 
  */
-increse.addEventListener('click', ()=>{
-    counterElement.textContent = increment(counterElement.textContent);
-})
-
-/**
- * Listen to decrement events
- */
-decrese.addEventListener('click', ()=>{
-    counterElement.textContent = decrement(counterElement.textContent);
-})
-
-/**
- * this function increments by one the input parameter
- * @param {string} counter value to be incremented
- * @returns {Number} return the incremented value
- */
-function increment(counter) { 
-    return parseInt(counter) + 1;
+function createButton(label, clickHandler) {
+    var button = document.createElement("button");
+    button.textContent = label;
+    button.onclick = clickHandler;
+    return button;
 }
 
 /**
- * this function decrements by one the input parameter
- * @param {string} counter value to be decremented
- * @returns {Number} return the decremented value
+ * This function checks whether the output element already exists, and if so, updates it; 
+ * otherwise, it creates a new output item and adds it to the "counterGrid"
+ * @param {number} value 
  */
-function decrement(counter) {
-    return parseInt(counter) - 1;
+function updateCounter(value) {
+    let counterElement = document.getElementById("output");
+    if (counterElement) {
+        counterElement.textContent = value;
+    } else {
+        let counterContainer = document.getElementById("counterGrid");
+        counterElement = document.createElement("div");
+        counterElement.id = "output";
+        counterElement.textContent = value;
+        counterContainer.appendChild(counterElement);
+    }
 }
+
+/**
+ * The number displayed on screen initialized at 0
+ */
+var currentCount = 0;
+
+/**
+ * This function increments the curretCount parameter by one and calls the updateCounter function 
+ */
+function increment() {
+    currentCount++;
+    updateCounter(currentCount);
+}
+
+/**
+ * This function decrements the curretCount parameter by one and calls the updateCounter function
+ */
+function decrement() {
+        currentCount--;
+        updateCounter(currentCount);
+}
+
+/**
+ * In this case the updateCounter function is used to initialize the div used to display the currentCount value 
+ */
+updateCounter(currentCount);
+
+/**
+ * creation of the + and - buttons
+ */
+var incrementButton = createButton("+", increment);
+var decrementButton = createButton("-", decrement);
+
+/**
+ * inserting button positions into HTML
+ */
+var counterContainer = document.getElementById("counterGrid");
+counterContainer.appendChild(incrementButton);
+counterContainer.appendChild(decrementButton);
+
 
